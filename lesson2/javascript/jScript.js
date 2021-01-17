323151325bb07aa368410dd748eae26f6b44f419
NinJimi <jefferymoser7@yahoo.com>
2021-01-16 23:00:32 -0800

    Design principle stuff

---
+++ lesson2/javascript/jScript.js
@@ -0,0 +1,6 @@
+var date = document.lastModified
+document.getElementById('modified').innerHTML = "Last updated: " + date;
+
+var today = new Date();
+var year = today.getFullYear();
+document.getElementById('year').innerHTML = year;
