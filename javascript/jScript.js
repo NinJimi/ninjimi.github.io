3fa075e091e53887831e6d15481ad595e343873b
NinJimi <jefferymoser7@yahoo.com>
2021-02-13 22:44:21 -0800

    I suck

--- javascript/jScript.js
+++ javascript/jScript.js
@@ -4,3 +4,11 @@ document.getElementById('modified').innerHTML = "Last updated: " + date;
 var today = new Date();
 var year = today.getFullYear();
 document.getElementById('year').innerHTML = year;
+
+WebFont.load({
+    google: {
+        families: [
+            'Oswald'
+        ]
+    }
+});
