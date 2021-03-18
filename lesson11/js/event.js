74e7a064dbbe012600223b0412611ff9dfb6d051
NinJimi <jefferymoser7@yahoo.com>
2021-03-17 22:31:49 -0700

    just need new pictures

--- lesson11/js/event.js
+++ lesson11/js/event.js
@@ -5,3 +5,25 @@ if (dayNumber == 5) {
 } else {
     document.getElementById("fri_message").classList.add("hideme");
 }
+
+const events = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(events)
+.then((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+    const towns = jsObject['towns']
+    if (document.URL.includes('preston.html')){
+        document.getElementById(`events${+1}`).textContent = towns[6].events[0];
+        document.getElementById(`events${+2}`).textContent = towns[6].events[1];
+        document.getElementById(`events${+3}`).textContent = towns[6].events[2];
+    } if (document.URL.includes('fishhaven.html')) {
+        document.getElementById(`events${+1}`).textContent = towns[2].events[0];
+        document.getElementById(`events${+2}`).textContent = towns[2].events[1];
+        document.getElementById(`events${+3}`).textContent = towns[2].events[2];
+        document.getElementById(`events${+4}`).textContent = towns[2].events[3];
+    } if (document.URL.includes('sodasprings.html')) {
+        document.getElementById(`events${+1}`).textContent = towns[0].events[0];
+        document.getElementById(`events${+2}`).textContent = towns[0].events[1];
+        document.getElementById(`events${+3}`).textContent = towns[0].events[2];
+    }
+});
--- lesson11/js/sodaspringsWeather.js
+++ lesson11/js/sodaspringsWeather.js
@@ -1,3 +1,16 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=9e918bd1e21cabf7a4c4b3edec366c4f"
+fetch(apiURL)
+.then((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-sky').textContent = jsObject.weather[0].main;
+    document.getElementById('current-high').textContent = jsObject.main.temp_max;
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('current-low').textContent = jsObject.main.temp_min;
+    document.getElementById('current-humidity').textContent = jsObject.main.humidity;
+    document.getElementById('current-windSpeed').textContent = jsObject.wind.speed;
+})
+
 const fiveDay = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=9e918bd1e21cabf7a4c4b3edec366c4f"
 fetch(fiveDay)
 .then((response) => response.json())
